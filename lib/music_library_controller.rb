8c31cee147699787101f50ffdfa0cbe26bffac9a
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-06-01 22:04:50 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -22,7 +22,16 @@ class MusicLibraryController
       case response 
         when "list songs"
           list_songs
-          
+        when "list artists"
+          list_artists
+        when "list genres"
+          list_genres 
+        when "list artist"
+          list_songs_by_artist
+        when "list genre"
+          list_songs_by_genre
+        when "play song" 
+          play_song
     end
   end
   
